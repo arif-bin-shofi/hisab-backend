56bd3368fde50b15a6f8fbf7d190cb49bbf29a62
arif-bin-shofi <ariftech559872@gmail.com>
2026-02-28 19:23:14 +0600

    add code

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import hisabRoutes from "./routes/hisabRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
-
 dotenv.config();
 
 const app = express();
@@ -15,6 +14,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* ===============================
+   🏠 Home Route
+================================= */
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "🚀 Welcome to HisabPro API Server",
+    
+  });
+});
+
+/* ===============================
+   🔗 API Routes
+================================= */
 app.use("/api/auth", authRoutes);
 app.use("/api/hisab", hisabRoutes);
 app.use("/api/customer", customerRoutes);
